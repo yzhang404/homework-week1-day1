85e819e9143dc0fc7b36423fe870540cefbda441
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-14 18:50:40 -0400

    Change the second heading when the button is clicked

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const button = document.querySelector('button')
-const h1 = document.querySelector('#greeting')
+const h1 = document.querySelector('#wonder')
 function changeHeading(){
-    h1.textContent = 'It is me'
-    
+    h1.textContent = 'It is me'   
 }
 button.addEventListener('click',changeHeading)
