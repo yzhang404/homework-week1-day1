3aa2a0b02954f314d61f37aca4324d70c041621f
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-14 19:20:33 -0400

    Create a new form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button')
 const h1 = document.querySelector('#wonder')
+
 function changeHeading(){
-    h1.textContent = 'It is me'   
+    h1.textContent = 'It is me'
 }
 button.addEventListener('click',changeHeading)
