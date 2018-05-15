f3449a6f41aa98dd818d74549e7f05a95fd4d687
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-15 14:30:02 -0400

    Add enter submit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const button = document.querySelector('button')
+const form = document.querySelector('#userForm')
 
+const handleSubmit = function(ev){
+    ev.preventDefault()
+    const heading =document.querySelector('h1#greeting')
+    const f = ev.target
+    const userName=f.value // target is the form today
+    heading.textContent= userName
+}
+
+form.addEventListener('submit',handleSubmit)
+/*
 function changeHeading(){
     const header = document.querySelector('h1#wonder')
     header.textContent = document.getElementById("text").value
 }
 
-button.addEventListener('click',changeHeading)
+button.addEventListener('click',changeHeading)*/
